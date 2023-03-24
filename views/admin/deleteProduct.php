f7c85043815d0e37d8476ff8407e126ac7bb996f
Youssef Sabir <sabiryoussef.officiel@gmail.com>
2023-03-24 20:35:27 +0000

    the admin dashpoard add,delet,update...

---
+++ views/admin/deleteProduct.php
@@ -0,0 +1,9 @@
+<?php
+if(isset($_SESSION["admin"]) && $_SESSION["admin"] == true){
+    $data = new ProductsController();
+    $data->removeProduct();
+}else{
+    Redirect::to("home");
+}
+                                                        
+                                                    
